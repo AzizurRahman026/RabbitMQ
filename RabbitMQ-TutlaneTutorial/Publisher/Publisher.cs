1458dfb4d6ada39a840779204be0941ce5d64795
Azizur Rahman <azizur.rahman@orbitax.com>
2024-12-12 13:07:29 +0600

    Rabbit code push

---
+++ RabbitMQ-TutlaneTutorial/Publisher/Publisher.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+// Connection parameters
+string UserName = "guest";
+string Password = "guest";
+string HostName = "localhost";
+
+var connectionFactory = new ConnectionFactory
+{
+    UserName = UserName,
+    Password = Password,
+    HostName = HostName
+};
+
+// Create an asynchronous connection
+var connection = await connectionFactory.CreateConnectionAsync(); // Await the connection
+
+// Create an asynchronous channel
+using var model = await connection.CreateChannelAsync(); // Await the channel to get IModel instance
+
+
+string exchangeName = "demoExchange";
+string exchangeType = ExchangeType.Direct;
+model.ExchangeDeclareAsync("demoExchange", exchangeType);
+
+Console.WriteLine($"Creating Exchange: {exchangeName} & type {exchangeType}");
+
+
+// Create Queue
+await model.QueueDeclareAsync(queue: "demoqueue", durable: true, exclusive: false, autoDelete: false,arguments: null);
+Console.WriteLine("Creating Queue");
+
+
+// Bind Queue to Exchange
+model.QueueBindAsync("demoqueue", "demoExchange", "directexchange_key");
+Console.WriteLine("Creating Binding");
+
+
+// byte[] messagebuffer = Encoding.Default.GetBytes("Direct Message");
+
+model.BasicPublishAsync(
+    exchange: exchangeName,
+    routingKey: "directexchange_key",
+    basicProperties: null,
+    body: Encoding.Default.GetBytes("hello")
+);
+
+Console.WriteLine("Message Sent");
+
---
+++ Work Queues/Worker/Worker.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null); // queue will not survive a RabbitMQ node restart
+// await channel.QueueDeclareAsync(queue: "hello", durable: true, exclusive: false, autoDelete: false, arguments: null); // will servive...
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    int dots = message.Split('.').Length - 1;
+    await Task.Delay(dots * 1000);
+
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+    Console.WriteLine(" [x] Done");
+
+    // here channel could also be accessed as ((AsyncEventingBasicConsumer)sender).Channel
+};
+
+await channel.BasicConsumeAsync("hello", autoAck: true, consumer: consumer);
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
