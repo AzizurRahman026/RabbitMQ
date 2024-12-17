456e911290aa7edd16492de513d707c8a14f8b20
Azizur Rahman <azizur.rahman@orbitax.com>
2024-12-17 17:10:45 +0600

    Rabbit direct exchange upload

---
+++ RabbitMQ-TutlaneTutorial/RabbitMQConsumer/MessageReceiver.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQConsumer
+{
+    public class MessageReceiver
+    {
+        private readonly IChannel _channel;
+
+        public MessageReceiver(IChannel channel)
+        {
+            _channel = channel;
+        }
+
+        public async Task StartConsumingAsync(string queueName)
+        {
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                Console.WriteLine($"Consuming Message");
+                Console.WriteLine(string.Concat("Message received from the exchange ", ea.Exchange));
+                Console.WriteLine(string.Concat("Consumer tag: ", ea.ConsumerTag));
+                Console.WriteLine(string.Concat("Delivery tag: ", ea.DeliveryTag));
+                Console.WriteLine(string.Concat("Routing tag: ", ea.RoutingKey));
+                Console.WriteLine(string.Concat("Message: ", ea.Body));
+
+                // Acknowledge the message
+                await _channel.BasicAckAsync(ea.DeliveryTag, false);
+            };
+
+            await _channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer: consumer);
+            Console.WriteLine($"Waiting for messages from queue: {queueName}");
+        }
+    }
+}
---
+++ RabbitMQ-TutlaneTutorial/RabbitMQConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQConsumer;
+using System.Threading.Channels;
+
+const string UName = "guest";
+const string Pwd = "guest";
+const string HName = "localhost";
+
+ConnectionFactory connectionFactory = new ConnectionFactory
+{
+    HostName = HName,
+    UserName = UName,
+    Password = Pwd,
+};
+var connection = await connectionFactory.CreateConnectionAsync();
+var channel = await connection.CreateChannelAsync();
+// accept only one unack-ed message at a time
+// uint prefetchSize, ushort prefetchCount, bool global
+await channel.BasicQosAsync(0, 1, false);
+MessageReceiver messageReceiver = new MessageReceiver(channel);
+
+// var messageReceiver = new AsyncEventingBasicConsumer(channel);
+
+/*if (messageReceiver != null)
+{
+    await channel.BasicConsumeAsync("request.queue", false, consumer: messageReceiver);
+    Console.ReadLine();
+}*/
+
+
+await messageReceiver.StartConsumingAsync("request.queue");
